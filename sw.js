463d21a1cd92a4f0ee57c00236d5859330225880
mms4-byte <marrwan0101@gmail.com>
2025-10-20 21:03:36 +0300

    Rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+// اسم الكاش
+const CACHE_NAME = 'six-pro-cache-v1';
+
+// الملفات التي سيتم تخزينها في الكاش
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://imgur.com/1l3hICE.png' // صورة الأيقونة من Imgur
+];
+
+// عند التثبيت: نحفظ الملفات في الكاش
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// عند التفعيل: نحذف أي كاش قديم
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// عند أي طلب: نرجع الملف من الكاش أولاً ثم من الإنترنت إذا لم يكن موجوداً
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
